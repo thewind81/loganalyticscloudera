70b78991c6e2afee20f74705ac79529f20d6f1c0
Hồ Hữu Trọng <thewind81@gmail.com>
2017-12-08 23:17:32 +0700

    Create cloudera-manager-installer.sh

---
+++ cloudera-manager-installer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt -y update
+apt -y upgrade
+sysctl -w vm.swappiness=10
+echo "vm.swappiness = 10" >> /etc/sysctl.conf
+wget https://archive.cloudera.com/cm5/installer/latest/cloudera-manager-installer.bin
+chmod u+x cloudera-manager-installer.bin
+./cloudera-manager-installer.bin
