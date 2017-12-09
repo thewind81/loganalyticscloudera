64861c1a83a9572f2d3d7cc33784a453ec276966
Hồ Hữu Trọng <thewind81@gmail.com>
2017-12-09 22:06:14 +0700

    Update cloudera-manager-installer.sh

--- cloudera-manager-installer.sh
+++ cloudera-manager-installer.sh
@@ -5,4 +5,4 @@ sysctl -w vm.swappiness=10
 echo "vm.swappiness = 10" >> /etc/sysctl.conf
 wget https://archive.cloudera.com/cm5/installer/latest/cloudera-manager-installer.bin
 chmod u+x cloudera-manager-installer.bin
-./cloudera-manager-installer.bin
+./cloudera-manager-installer.bin --i-agree-to-all-licenses --noprompt --noreadme --nooptions
